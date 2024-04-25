4953572023448c2e3ca5c49d94542a4bde1c8dd3
Drm123 <doremyous@gmail.com>
2024-04-25 20:35:06 +0800

    Site updated: 2024-04-25 20:35:06

--- js/fomal.js
+++ js/fomal.js
@@ -923,6 +923,53 @@ function switchNightMode() {
   typeof utterancesTheme === 'function' && utterancesTheme()
   typeof FB === 'object' && window.loadFBComment()
   window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
+
+
+  // 这里为了统一颜色选取的是“明暗模式”下的两种字体颜色，也可以自己定义
+  let color = document.documentElement.getAttribute('data-theme') === 'light' ? 'rgb(154,154,251)' : 'rgb(142,193,253)'
+  if (document.getElementById('posts-chart') && postsOption) {
+    try {
+      let postsOptionNew = postsOption
+      postsOptionNew.title.textStyle.color = color
+      postsOptionNew.xAxis.nameTextStyle.color = color
+      postsOptionNew.yAxis.nameTextStyle.color = color
+      postsOptionNew.xAxis.axisLabel.color = color
+      postsOptionNew.yAxis.axisLabel.color = color
+      postsOptionNew.xAxis.axisLine.lineStyle.color = color
+      postsOptionNew.yAxis.axisLine.lineStyle.color = color
+      postsOptionNew.series[0].markLine.data[0].label.color = color
+      postsChart.setOption(postsOptionNew)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  if (document.getElementById('tags-chart') && tagsOption) {
+    try {
+      let tagsOptionNew = tagsOption
+      tagsOptionNew.title.textStyle.color = color
+      tagsOptionNew.xAxis.nameTextStyle.color = color
+      tagsOptionNew.yAxis.nameTextStyle.color = color
+      tagsOptionNew.xAxis.axisLabel.color = color
+      tagsOptionNew.yAxis.axisLabel.color = color
+      tagsOptionNew.xAxis.axisLine.lineStyle.color = color
+      tagsOptionNew.yAxis.axisLine.lineStyle.color = color
+      tagsOptionNew.series[0].markLine.data[0].label.color = color
+      tagsChart.setOption(tagsOptionNew)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  if (document.getElementById('categories-chart') && categoriesOption) {
+    try {
+      let categoriesOptionNew = categoriesOption
+      categoriesOptionNew.title.textStyle.color = color
+      categoriesOptionNew.legend.textStyle.color = color
+      if (!categoryParentFlag) { categoriesOptionNew.series[0].label.color = color }
+      categoriesChart.setOption(categoriesOptionNew)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
 
 /* 夜间模式切换动画 end */
